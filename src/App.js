b3b1542306727074eed14135aa42a7ced813b8eb
Michael LeCompte <michaelslec98@gmail.com>
2020-06-09 16:37:19 -0700

    Cleaned up old code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { Elements, CardElement, useStripe, useElements, ElementsConsumer } from '@stripe/react-stripe-js';
-import { loadStripe } from '@stripe/stripe-js';
+import { useAsync } from 'react-async';
 import logo from './logo.svg';
 import './App.css';
-import { validate } from 'json-schema';
-import { useAsync } from 'react-async';
 
-// const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_TEST_API_KEY);
-
-const stripe = require('stripe')(process.env.REACT_APP_STRIPE_API_KEY);
+let params = { limit: 20 }
 const stripe_auth = { "Authorization": `Bearer ${process.env.REACT_APP_STRIPE_API_KEY}` }
-let params = { limit: 11 }
 const stripe_url = `https://api.stripe.com/v1/customers?limit=${params.limit}`;
-
 
 // Then we'll fetch user data from this API
 
@@ -23,29 +16,29 @@ const loadUsers = async () =>
     method: "GET",
     headers: stripe_auth
   }).then(res => (res.ok ? res : Promise.reject(res)))
-    .then(res => res.json())
+    .then(res => res.json());
 
 
 function App() {
-  const { data: all_customers, error, isLoading } = useAsync({ promiseFn: loadUsers })
-
-
+  const { data: all_customers, error, isLoading } = useAsync({ promiseFn: loadUsers });
   if (isLoading) return "Loading..."
   if (error) return `Something went wrong: ${error.message}`
+
+  // Get only unique by expanding a Set of the same information
+  // let unique = [...new Set(all_customers.data.map(item => item.email))];
+
   if (all_customers)
-
-    // let unique = [...new Set(data.map(item => item.Group))];
-
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>{all_customers.data.map(user => (
-            <p>{user.email}</p>
+          <p>{all_customers.data.map(email => (
+            <p>{email.email}</p>
           ))}</p>
           <p>
-            Qazi is working on this app right now... <code>src/App.js</code> and save to reload.
-        </p>
+            Qazi is working on this app right now... <code>src/App.js</code> and
+            save to reload.
+          </p>
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -53,7 +46,7 @@ function App() {
             rel="noopener noreferrer"
           >
             Learn React
-        </a>
+          </a>
         </header>
 
       </div>
