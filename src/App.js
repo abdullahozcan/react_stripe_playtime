916cf02b9fd73873bd7a77d5a782a55117605602
Rafeh Qazi <qazi@cleverprogrammer.com>
2020-06-09 14:23:39 -0700

    Sharing with Michael.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Elements, CardElement, useStripe, useElements, ElementsConsumer } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import logo from './logo.svg';
 import './App.css';
+import { validate } from 'json-schema';
 
-const STRIPE_API_KEY = process.env.REACT_APP_STRIPE_API_KEY
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_TEST_API_KEY);
+
+const stripe = require('stripe')(process.env.REACT_APP_STRIPE_TEST_API_KEY);
+
+async function fetchCustomer() {
+  var customerData = await stripe.customers.retrieve('cus_HPa9luwqRekiTv',
+    function (err, customer) {
+      // asynchronously called
+      console.log('🔥🔥🔥', customer);
+      return customer;
+    })
+
+  console.log(customerData);
+  return customerData;
+  // setCustomer1(customerData);
+}
+
+console.log(fetchCustomer());
+
+
 
 function App() {
+  const [customer1, setCustomer1] = useState([]);
+
+  useEffect(() => {
+    async function fetchCustomer() {
+      var customerData = await stripe.customers.retrieve('cus_HPa9luwqRekiTv',
+        function (err, customer) {
+          // asynchronously called
+          console.log('🔥🔥🔥', customer);
+          return customer;
+        })
+
+      console.log(customerData);
+      setCustomer1(customerData);
+      console.log(customerData);
+    }
+
+    fetchCustomer();
+  }, [])
+
   return (
+
     <div className="App">
+      <Elements stripe={stripePromise}>
+        <p>My stripe thing is loaded</p>
+      </Elements>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
+
         <p>
           Qazi is working on this app right now... <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/checkoutMinimal.js
@@ -0,0 +1,105 @@
+import React, { useState, useEffect } from 'react';
+import { Elements, CardElement, useStripe, useElements, ElementsConsumer } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import logo from './logo.svg';
+import './App.css';
+
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_TEST_API_KEY);
+
+class CheckoutForm extends React.Component {
+  handleSubmit = async (event) => {
+    // Block native form submission.
+    event.preventDefault();
+
+    const { stripe, elements } = this.props;
+
+    if (!stripe || !elements) {
+      // Stripe.js has not loaded yet. Make sure to disable
+      // form submission until Stripe.js has loaded.
+      return;
+    }
+
+    // Get a reference to a mounted CardElement. Elements knows how
+    // to find your CardElement because there can only ever be one of
+    // each type of element.
+    const cardElement = elements.getElement(CardElement);
+
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      type: 'card',
+      card: cardElement,
+    });
+
+    if (error) {
+      console.log('[error]', error);
+    } else {
+      console.log('[PaymentMethod]', paymentMethod);
+    }
+  };
+
+  render() {
+    const { stripe } = this.props;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: '16px',
+                color: '#424770',
+                '::placeholder': {
+                  color: '#aab7c4',
+                },
+              },
+              invalid: {
+                color: '#9e2146',
+              },
+            },
+          }}
+        />
+        <button type="submit" disabled={!stripe}>
+          Pay
+        </button>
+      </form>
+    );
+  }
+}
+
+const InjectedCheckoutForm = () => {
+  return (
+    <ElementsConsumer>
+      {({ elements, stripe }) => (
+        <CheckoutForm elements={elements} stripe={stripe} />
+      )}
+    </ElementsConsumer>
+  );
+};
+
+function App() {
+  return (
+
+    <div className="App">
+      <Elements stripe={stripePromise}>
+        <p>My stripe thing is loaded</p>
+        <InjectedCheckoutForm />
+      </Elements>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+
+
+        <p>
+          Qazi is working on this app right now... <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  )
+}
+
+export default App;
