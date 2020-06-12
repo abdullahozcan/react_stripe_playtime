19bb0800b721908880340ca6b534b72268de01a0
Rafeh Qazi <qazi@cleverprogrammer.com>
2020-06-12 02:08:57 -0700

    Add k payment method.

--- src/App.js
+++ src/App.js
@@ -2,51 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import Search from './components/Search';
 import SnackyBar from './components/SnackyBar';
-import LogRocket from 'logrocket';
 import StripeList from './components/StripeList';
-import { StaticKitProvider } from '@statickit/react';
 import { loadStripe } from '@stripe/stripe-js';
-import { Elements } from '@stripe/react-stripe-js';
-import FixedCharge from './components/FixedCharge';
 import BasicCheckout from './components/BasicCheckout';
 import Box from '@material-ui/core/Box';
-import Grid from '@material-ui/core/Grid';
 import PsCheckout500 from './components/PsCheckout500';
 import PsCheckoutDeposit500 from './components/PsCheckoutDeposit500';
+import PsCheckout3k from './components/PsCheckout3k';
 
 
 // This will inject the Stripe.js script on your site for you
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_TEST_API_KEY);
 
-LogRocket.init('usduoh/clever-programmer-react-apps');
-
-LogRocket.identify('123456', {
-  name: 'Sonic the Hedgehog',
-  email: 'gottacodefast@gmail.com',
-
-  subscriptionType: 'premium',
-  favoriteFood: 'chili dogs'
-});
-
-
-let params = { limit: 20 }
-const stripe_auth = { "Authorization": `Bearer ${process.env.REACT_APP_STRIPE_API_KEY}` }
-const stripe_url = `https://api.stripe.com/v1/customers?limit=${params.limit}`;
-
-
-// https://css-tricks.com/fetching-data-in-react-using-react-async/
-// https://www.freecodecamp.org/forum/t/fetch-api-with-api-key/317584
-const loadCustomers = async () =>
-  await fetch(stripe_url, {
-    method: "GET",
-    headers: stripe_auth
-  }).then(res => (res.ok ? res : Promise.reject(res)))
-    .then(res => res.json());
-
-
-function App({ Component, pageProps }) {
-
-
+function App() {
   return (
 
     <div className="App">
@@ -56,23 +24,11 @@ function App({ Component, pageProps }) {
         <h2>Welcome to <span style={{ color: "#ff9d00" }}>Clever Programmer </span> Headquarters 🚀</h2>
         <span><BasicCheckout /> <PsCheckoutDeposit500 /></span>
         <br />
-        <PsCheckout500 />
+        <span><PsCheckout3k /> <PsCheckout500 /></span>
         <Box m="1rem" />
         <StripeList />
         <Box m="1rem" />
         <SnackyBar />
-
-
-        {/* <StaticKitProvider site={String(process.env.siteId)}>
-          <Elements stripe={stripePromise}>
-
-            <div className="antialiased bg-gray-900 h-full min-h-screen">
-
-              <FixedCharge />
-            </div>
-          </Elements>
-        </StaticKitProvider> */}
-
         <Search />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -88,7 +44,6 @@ function App({ Component, pageProps }) {
           Learn React
           </a>
       </header>
-
 
     </div>
   )
---
+++ src/components/PsCheckout3k.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { loadStripe } from '@stripe/stripe-js';
+import Button from "@material-ui/core/Button";
+// Make sure to call `loadStripe` outside of a component’s render to avoid
+// recreating the `Stripe` object on every render.
+const stripePromise = loadStripe('pk_test_IYu0gaTp2CveAO2Oud1KiQzS');
+
+function PsCheckout500() {
+    const handleClick = async (event) => {
+        // When the customer clicks on the button, redirect them to Checkout.
+        const stripe = await stripePromise;
+        const { error } = await stripe.redirectToCheckout({
+            lineItems: [
+                // Replace with the ID of your price
+                { price: 'price_1Gt8nPAmH5VbdPbpaSBek0Sa', quantity: 1 },
+            ],
+            mode: 'payment',
+            successUrl: 'https://example.com/success',
+            cancelUrl: 'https://example.com/cancel',
+        });
+        // If `redirectToCheckout` fails due to a browser or network
+        // error, display the localized error message to your customer
+        // using `error.message`.
+    };
+    return (
+        <Button variant="contained" color="primary" role="link" onClick={handleClick}>
+            💰 1 Payment of $3,000
+    </Button>
+    );
+}
+
+export default PsCheckout500;
